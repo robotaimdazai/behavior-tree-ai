4f7bb57964ef8a58b10dd361886ffbb1021fa3ce
Bilal Azhar <90259416+robotaimdazai@users.noreply.github.com>
2023-05-01 12:56:47 +0500

    added base classes

---
+++ Assets/Btree/Scripts/Base/Tree.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+namespace BTree
+{
+    public abstract class Tree : MonoBehaviour
+    {
+        private Node _root = null;
+        protected abstract Node SetupTree();
+        
+        public Node Root => _root;
+        
+        protected void Start()
+        {
+            _root = SetupTree();
+        }
+
+        private void Update()
+        {
+            if (_root != null)
+                _root.Evaluate();
+        }
+
+    }
+}
+
